a14f0d12133fa8ade9c4fb19d837e99fbf8338b2
Ameer jafar <ameerjafar123@gmail.com>
2024-03-04 12:00:34 +0530

    adding the code

--- backend/middleware/blog.js
+++ backend/middleware/blog.js
@@ -1,4 +1,4 @@
-import { Blog } from '../config/db'
+const { Blog } = require('../config/db');
 
 const gettingAllBlogs = async (req, res, next) => {
     const userId = req.body.userId;
@@ -7,6 +7,18 @@ const gettingAllBlogs = async (req, res, next) => {
     });
     res.status(200).send({message: userBlogs});
 }
+const addBlog = async (req, res, next) => {
+    const userId = req.body.userId;
+    const title = req.body.title;
+    const content = req.body.content;
+    const addblog = await Blog.create({
+        userId: userId,
+        title: title,
+        content: content
+    })
+    res.status(200).send({message: "your blog is added successfully into the database"});
+}
 module.exports = {
-   gettingAllBlogs
+    gettingAllBlogs,
+    addBlog
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
+
 const  { signupMiddleware } = require('../middleware/signup');
 const { signinMiddleware } = require('../middleware/signin');
 const { gettingAllBlogs } = require('../middleware/blog');
+const { addBlog } = require('../middleware/blog');
+
 router.post('/signup', signupMiddleware ,(req, res) => {
     res.status(200).send({message: 'signup successfully'});
 });
 router.post('/signin', signinMiddleware, (req, res) => {
     res.status(200).send({message: 'signin successfully'});
 })
-router.get('/blog', gettingAllBlogs ,(req, res) => {
-
+router.post('/addblog', addBlog ,(req, res) => {
+    res.status(200).send({message: 'This is from addblog section'});
+})
+router.post('/blog', gettingAllBlogs ,(req, res) => {
     res.status(200).send({message: 'we are in blog page'});
 })
 module.exports = router; 
