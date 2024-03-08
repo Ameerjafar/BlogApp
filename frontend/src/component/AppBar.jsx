b59fea6dbdb6f9d0645a8652edbbbd50b9954710
Ameer jafar <ameerjafar123@gmail.com>
2024-03-08 21:14:31 +0530

    adding code

--- frontend/src/component/AppBar.jsx
+++ frontend/src/component/AppBar.jsx
@@ -7,7 +7,7 @@ export function AppBar() {
                 <div className = 'w-4/5'>
                     <div className="rounded-full w-34 font-bold pl-2">medium</div>
                 </div>
-                <button onClick = {writeHandler} class="fa fa-pencil" aria-hidden="true"></button>
+                <button onClick = {writeHandler} className="fa fa-pencil" aria-hidden="true"></button>
                 <button class="fa fa-bell" aria-hidden="true"></button>
                 <button className = 'w-6 h-6 rounded-full bg-gray-300 text-white'>AJ</button>
         </div>
--- frontend/src/component/ShowBlog.jsx
+++ frontend/src/component/ShowBlog.jsx
@@ -10,8 +10,10 @@ export function ShowBlog() {
                 <div className = 'font-bold text-2xl w-3/5'>{title}</div>
                 <div className = 'p-2'>Author</div>
             </div>
-            <div>
-                <div>This is my content</div>
+            <div className = 'flex'>
+                <div className = 'font-normal w-3/5'>This is my content</div>
+                <div className = 'flex items-center justify-center p-2 w-8 h-8 bg-gray-600 rounded-full'>A</div>
+                <div className = 'text-sm p-2 w-20'>In this one we are going to share the author information</div>
             </div>
         </div>
     </div>
