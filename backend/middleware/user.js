54aeaab5d80f53a53af417e9cd3c60062bea3435
Ameer jafar <ameerjafar123@gmail.com>
2024-03-10 11:53:27 +0530

    almost completed the project

--- backend/middleware/user.js
+++ backend/middleware/user.js
@@ -3,9 +3,10 @@ const { User } = require('../config/db');
 const user = async (req, res, next) => {
     const userId = req.body.userId;
     const response = await User.findOne({ _id: userId});
+    console.log(response);
     res.status(200).send({
-        firstName: response.firstName,
-        lastName: response.lastName
+        firstName: response['firstName'],
+        lastName: response['lastName']
     })
 }
 module.exports = {
